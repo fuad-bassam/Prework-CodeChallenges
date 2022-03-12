6401e6fa6da86416183a9e5774affe06779be7a3
fuad-bassam <74737369+fuad-bassam@users.noreply.github.com>
2022-03-12 23:52:45 +0300

    ch2 solve

--- Program/Program.cs
+++ Program/Program.cs
@@ -21,18 +21,14 @@ namespace Program
 
             return arr;
 
-
         }
 
         public static int MaxArr(int[] numArr, int sNum)
-
-           
         {
             int x = 0;
 
             foreach (int s in numArr)
             {
-
                 if (s == sNum)
                 {
                     x += s;
@@ -44,18 +40,59 @@ namespace Program
 
         }
 
+        public static void LeapYearCalculator(int x) {
 
+
+
+            if (x % 4 == 0)
+            {
+
+                if (x % 400 == 0)
+                {
+                    Console.WriteLine(x + " is a leap year, because this year is divisible by 400 and by 4");
+                }
+                else if (x % 100 == 0)
+                {
+                    Console.WriteLine(x + " is NOT a leap year, because this year is divisible by 100");
+                }
+                else
+                {
+
+                    Console.WriteLine(x + " is a leap year, because this year is divisible by 4");
+                }
+
+
+            }
+            else {
+                Console.WriteLine(x + " is NOT a leap year, because this year is NOT divisible by 4");
+            }
+        
+
+        }
 
         static void Main(string[] args)
         {
+
+           Console.WriteLine("Problem 1: Array Max Result");
 
             int[] a = CreatArr();
             Console.Write("Enter Number that you wont to search for :- ");
             int x = Convert.ToInt32(Console.ReadLine());
 
-
-
             Console.WriteLine(MaxArr(a, x));
+
+            /////////////////////////////////////////////////////////
+            
+            Console.WriteLine(" ");
+            Console.WriteLine("==========================");
+            Console.WriteLine("Problem 2: Leap Year Calculator");
+
+            
+           
+            Console.Write("\n Enter the year you wont :- ");
+            LeapYearCalculator(Convert.ToInt32(Console.ReadLine()));
+
+
 
         }
     }
